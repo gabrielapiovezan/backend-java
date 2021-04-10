692603765ce6e081c501c2b9785f909b59be7ef7
GabrielaPiovezan <gabrielapiovezan@live.com>
2021-04-10 19:38:29 -0300

    Order, Instant

--- src/main/java/com/project/backend/entities/User.java
+++ src/main/java/com/project/backend/entities/User.java
@@ -1,12 +1,18 @@
 package com.project.backend.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 @Entity
+@Table(name="tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -19,6 +25,11 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy ="client")
+	List<Order> orders = new ArrayList<>();
+	
+
+
 	public User () {
 	}
 
@@ -70,7 +81,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
