2535777bab04086672fdaef7a37e6f40900e8709
GabrielaPiovezan <gabrielapiovezan@live.com>
2021-04-12 01:29:05 -0300

    Exception handling - delete

--- src/main/java/com/project/backend/services/UserService.java
+++ src/main/java/com/project/backend/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.project.backend.entities.User;
 import com.project.backend.repositories.UserRepository;
+import com.project.backend.services.exceptions.DatabaseException;
 import com.project.backend.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -15,23 +18,29 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
-	 Optional<User> obj = repository.findById(id);
-	 return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		 repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
@@ -41,6 +50,6 @@ public class UserService {
 	private void updateData(User entity, User obj) {
 		entity.setEmail(obj.getEmail());
 		entity.setName(obj.getName());
-		entity.setPhone(obj.getPhone());		
+		entity.setPhone(obj.getPhone());
 	}
 }
