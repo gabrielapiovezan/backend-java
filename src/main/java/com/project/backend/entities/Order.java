fac644b3c44c8f004192c7d334945683b19db34b
GabrielaPiovezan <gabrielapiovezan@live.com>
2021-04-11 21:35:17 -0300

    Subtotal & Total methods

--- src/main/java/com/project/backend/entities/Order.java
+++ src/main/java/com/project/backend/entities/Order.java
@@ -100,6 +100,14 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
+	public Double getTotal() {
+		Double sum = 0.0;
+		for(OrderItem orderItem : items) {
+			sum += orderItem.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
